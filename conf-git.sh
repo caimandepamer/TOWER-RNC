07475c129123a141238506e53ffce9c34057b9f6
ibmrcampove - ibmrcampovelap03 <ibmrcampove@ibmrcampovelap03>
2021-01-18 04:59:07 -0500

    conf de git

--- conf-git.sh
+++ conf-git.sh
@@ -1,3 +1,21 @@
- git config --global user.name "soporte01 - ansiblecos702"
- git config --global user.email soporte01@ansiblecos702
+git config --global user.name "$USER - $HOSTNAME"
+git config --global user.email "$USER"@"$HOSTNAME"
 git config --global color.ui true
+
+#--- mini guia ---#
+# Actualizar el repo local (con la info en git)
+# git pull
+
+# Agregar archivos a la lista para ser subidor a git
+# git add --all
+
+# Ver que archivos estan listos para ser subidos a git
+# git status
+
+# Sincronizar los cambios en files con el repo LOCAL (aun no git)
+#    Permitiendo agregar un comentario
+# git commit -m 'Initial commit of all files to the repository'
+
+# Subir cambios a git
+# git push
+
